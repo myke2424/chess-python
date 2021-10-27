ea87eaf8cab9fc4d51e952f8afb648d2e8bdbf38
myke2424 <myke2424@gmail.com>
2021-10-27 13:05:29 -0400

    Refactored gui class

--- chess/gui.py
+++ chess/gui.py
@@ -6,18 +6,21 @@ from typing import List
 
 
 class GUI:
-    """ Responsible for drawing the GUI (board, pieces, highlighting etc)"""
+    """ Responsible for drawing the GUI (board, pieces, highlighting etc) """
 
     def __init__(self, settings: Settings, board: List[List[int]]):
         self.board = board
         self.screen = pygame.display.set_mode((settings.WIDTH, settings.HEIGHT))
         self.square_size = settings.SQUARE_SIZE
         self.board_colors = [pygame.Color(color) for color in settings.BOARD_COLORS]
+        self.piece_images = {}
+
+        self._load_piece_images(image_dir=settings.IMAGE_DIR)
 
     def draw(self) -> None:
         """ Main interface used to draw the entire gui """
         self._draw_board()
-        # self.draw_pieces()
+        self._draw_pieces()
 
     def _draw_square(self, row: int, col: int) -> None:
         """ Draw one square on the board for the given row/col """
@@ -30,3 +33,38 @@ class GUI:
         for row in range(8):
             for col in range(8):
                 self._draw_square(row, col)
+
+    def _draw_piece(self, row: int, col: int) -> None:
+        """ Draw the piece on the given square (row/col) """
+        piece = self.board[row][col]
+        # TODO: Refactor to an enum?
+        # If the piece isn't an empty square ('**' signifies an empty square)
+        if piece != '**':
+            square = pygame.Rect(col * self.square_size, row * self.square_size, self.square_size, self.square_size)
+            # 'blit' draws the image on the screen on the given square
+            self.screen.blit(self.piece_images[piece], square)
+
+    def _draw_pieces(self) -> None:
+        """ Draw all the pieces on the chess board """
+        for row in range(8):
+            for col in range(8):
+                self._draw_piece(row, col)
+
+    @staticmethod
+    def _load_and_scale_image(image_path: str, width: int, height: int) -> pygame.Surface:
+        """ Load and re-scale the image to the specified width/height """
+        return pygame.transform.scale(pygame.image.load(os.path.abspath(image_path)), (width, height))
+
+    def _load_piece_images(self, image_dir: str) -> None:
+        """ Load all the piece images into self.peace_images dict. """
+
+        if not os.path.exists(image_dir):
+            print(f"Image path {image_dir} isn't valid")
+        images = os.listdir(image_dir)
+
+        for image_path in images:
+            piece = image_path.split('.')[0]
+            loaded_piece_image = self._load_and_scale_image(image_path=f"{image_dir}/{image_path}",
+                                                            width=self.square_size,
+                                                            height=self.square_size)
+            self.piece_images[piece] = loaded_piece_image
