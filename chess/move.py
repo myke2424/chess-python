7a1d17b83eaa9db868fbd939745219365a6bf56e
myke2424 <myke2424@gmail.com>
2021-10-27 17:17:52 -0400

    Updated state class, added move abstraction

---
+++ chess/move.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Move:
+    """ Abstraction that represents a player move """
+
+    def __init__(self, starting_square: tuple, destination_square: tuple, board: List[List[str]]):
+        self.start_row = starting_square[0]
+        self.start_col = starting_square[1]
+        self.dest_row = destination_square[0]
+        self.dest_col = destination_square[1]
+        self.piece_to_move = board[self.start_row][self.start_col]
+        self.piece_to_capture = board[self.dest_row][self.dest_col]  # This can be an empty square
---
+++ chess/state.py
@@ -0,0 +1,44 @@
+from move import Move
+
+
+class GameState:
+    """
+    Responsible for storing/updating all the information about the current state of the game.
+    Update state (make moves), record move log, keep track of who's got the piece advantage etc."
+    """
+
+    def __init__(self):
+        """
+        board: 8x8 matrix. Each cell has two characters, first char represents the color of the piece,
+        second char represents the type. e.g. 'bK' = black king.
+        '**' represents empty squares. Each cell has a corresponding image with the same name.
+        """
+
+        self.board = [
+            ['bR', 'bN', 'bB', 'bQ', 'bK', 'bB', 'bN', 'bR'],
+            ['bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP'],
+            ['**', '**', '**', '**', '**', '**', '**', '**'],
+            ['**', '**', '**', '**', '**', '**', '**', '**'],
+            ['**', '**', '**', '**', '**', '**', '**', '**'],
+            ['**', '**', '**', '**', '**', '**', '**', '**'],
+            ['wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP'],
+            ['wR', 'wN', 'wB', 'wQ', 'wK', 'wB', 'wN', 'wR']
+        ]
+        self.move_log = []
+        self.white_to_move = True
+
+    def __repr__(self) -> str:
+        # TODO: Print unicode chess board
+        return str(self.board)
+
+    def make_move(self, move: Move) -> None:
+        """ Move a piece on the chess board """
+        # Makes original spot empty since we're moving the piece
+        self._make_square_empty(row=move.start_row, col=move.start_col)
+        self.board[move.dest_row][move.dest_col] = move.piece_to_move  # Move the piece
+        self.move_log.append(move)
+        self.white_to_move = False
+
+    def _make_square_empty(self, row: int, col: int) -> None:
+        """ Render the square empty on the given row/col"""
+        self.board[row][col] = '**'
