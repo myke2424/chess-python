73b89102cb29583ba51b73ee5ab7b3b14b298be5
myke2424 <myke2424@gmail.com>
2021-10-28 11:47:17 -0400

    Added chess notation parser for rows/cols

--- chess/move.py
+++ chess/move.py
@@ -17,4 +17,35 @@ class Move:
         color = self.piece_to_move[0]
         piece_type = self.piece_to_move[1]
 
-        return f"row: {self.start_row}, col: {self.start_col}"
+        # Bottom left of the board = Row 7, Col 0
+        # Bottom right of board = Row 7, Col 7
+        # Top left of board = Row 0, Col, 0
+        # Top right of the board = Row 0, Col 7
+
+        return _ChessNotationParser.get_chess_notation_from_row_and_col(row=self.dest_row, col=self.dest_col)
+
+    @classmethod
+    def from_chess_notation(cls, starting_square: tuple, destination_square: tuple, board: List[List[str]]) -> 'Move':
+        """ Alternate constructor to create a move obj from standard chess notation """
+        pass
+
+
+class _ChessNotationParser:
+    """
+    On a chess board, rows  are called RANKS (horizontal)
+    On a chess board, columns are called FILES (vertical)
+    A square is represented by the FILE character then RANK number
+    """
+    rows_to_ranks = {7: "1", 6: "2", 5: "3", 4: "4", 3: "5", 2: "6", 1: "7", 0: "8"}
+    columns_to_files = {0: "a", 1: "b", 2: "c", 3: "d", 4: "e", 5: "f", 6: "g", 7: "h"}
+
+    @classmethod
+    def get_chess_notation_from_row_and_col(cls, row: int, col: int) -> str:
+        """
+        Given a row/col, return its position on the board in standard chess notation
+        e.g. row=7 col=0 -> a1
+        row 7 would be the last row in the board matrix, but from a player perspective it would be there first row
+        col 0 would be the first col of the board, columns are in alphabetical order, resulting in 'a'
+        """
+        chess_notation = cls.columns_to_files.get(col) + cls.rows_to_ranks.get(row)
+        return chess_notation
