12a47d9e8be33c7806572d35869ac2416f117704
myke2424 <myke2424@gmail.com>
2021-10-29 15:37:07 -0400

    Started generating all possible moves for a pawn

--- chess/piece.py
+++ chess/piece.py
@@ -1,6 +1,12 @@
-from square import Square
-from chess_notation import ChessNotationParser
+from abc import ABC, abstractmethod
 from enum import Enum
+from typing import List
+
+from chess_notation import ChessNotationParser
+from move import Move
+from square import Square
+
+EMPTY_SQUARE = "**"
 
 
 class Color(Enum):
@@ -8,28 +14,40 @@ class Color(Enum):
     BLACK = "b"
 
 
-class Piece:
+class Piece(ABC):
     def __init__(self, color: str, row: int, col: int):
         self.color = Color(color)
         self.pos = Square(row, col)
         self.moves_made = 0
         self.name = color + self.__class__.__name__  # name is color + piece type, e.g. bKnight
 
+    def __repr__(self) -> str:
+        """ Return the piece name/color  """
+        type_ = self.__class__.__name__
+        color = "Black" if self.color == Color.BLACK else "White"
+
+        return f"{color} {type_}"
+
+    def __eq__(self, other: "Piece") -> bool:
+        """ Checks if two pieces are equal """
+        if isinstance(other, Piece):
+            if self.name == other.name and self.pos.row == other.pos.row and self.pos.col == other.pos.col:
+                return True
+        return False
+
     @property
     def pos_chess_notation(self) -> str:
         """ Position on the chess board in standard chess notation """
         return ChessNotationParser.from_row_and_col(self.pos.row, self.pos.col)
 
-    def possible_moves(self) -> list:
-        """ Polymorphic function we can use to generate all possible moves for the piece """
+    def update_position(self, destination_square: Square) -> None:
+        """ Update the pieces position to the destination square """
+        self.pos = destination_square
+
+    @abstractmethod
+    def possible_moves(self, board) -> List[Move]:
+        """ Interface we can use to generate all possible moves for the piece given the current state of the board """
         pass
-
-    def __repr__(self) -> str:
-        """ Return the piece name/color and position on the board in chess notation """
-        type_ = self.__class__.__name__
-        color = "Black" if self.color == "b" else "White"
-
-        return f"{color} {type_} {self.pos_chess_notation}"
 
 
 class Pawn(Piece):
@@ -38,9 +56,38 @@ class Pawn(Piece):
     def __init__(self, color: str, row: int, col: int):
         super().__init__(color=color, row=row, col=col)
 
-    def possible_moves(self):
-        """ Get all the pawn moves for the pawn in its current position on the board """
-        pass
+    # White pawns move UP the board
+    # Black pawns move DOWN the board
+    # Can't move backwards
+    # Pawn can move two squares on the first move
+    # Capture Diagonally
+    def possible_moves(self, board) -> List[Move]:
+        """ Get all the pawn moves for its current position on the board """
+        moves = []
+        if self.color == Color.WHITE:
+            # Since we're white, check the square above us and see if its empty
+            if board[self.pos.row - 1][self.pos.col] == EMPTY_SQUARE:  # 1 square pawn advance
+                square_to_land_on = Square(row=self.pos.row - 1, col=self.pos.col)
+                move = Move(starting_square=self.pos, destination_square=square_to_land_on, board=board)
+                moves.append(move)
+
+                # Check if the second square above is free (2 square pawn advance)
+                if self.moves_made == 0 and board[self.pos.row - 2][self.pos.col] == EMPTY_SQUARE:
+                    square_to_land_on = Square(row=self.pos.row - 2, col=self.pos.col)
+                    move = Move(starting_square=self.pos, destination_square=square_to_land_on, board=board)
+                    moves.append(move)
+        elif self.color == Color.BLACK:
+            if board[self.pos.row + 1][self.pos.col] == EMPTY_SQUARE:  # 1 square pawn advance
+                square_to_land_on = Square(row=self.pos.row + 1, col=self.pos.col)
+                move = Move(starting_square=self.pos, destination_square=square_to_land_on, board=board)
+                moves.append(move)
+
+                # Check if the second square above is free (2 square pawn advance)
+                if self.moves_made == 0 and board[self.pos.row + 2][self.pos.col] == EMPTY_SQUARE:
+                    square_to_land_on = Square(row=self.pos.row + 2, col=self.pos.col)
+                    move = Move(starting_square=self.pos, destination_square=square_to_land_on, board=board)
+                    moves.append(move)
+        return moves
 
 
 class Bishop(Piece):
@@ -49,12 +96,18 @@ class Bishop(Piece):
     def __init__(self, color: str, row: int, col: int):
         super().__init__(color=color, row=row, col=col)
 
+    def possible_moves(self, board) -> List[Move]:
+        return []
+
 
 class Knight(Piece):
     value = 3
 
     def __init__(self, color: str, row: int, col: int):
         super().__init__(color=color, row=row, col=col)
+
+    def possible_moves(self, board) -> List[Move]:
+        return []
 
 
 class Rook(Piece):
@@ -63,6 +116,9 @@ class Rook(Piece):
     def __init__(self, color: str, row: int, col: int):
         super().__init__(color=color, row=row, col=col)
 
+    def possible_moves(self, board) -> List[Move]:
+        return []
+
 
 class Queen(Piece):
     value = 9
@@ -70,9 +126,15 @@ class Queen(Piece):
     def __init__(self, color: str, row: int, col: int):
         super().__init__(color=color, row=row, col=col)
 
+    def possible_moves(self, board) -> List[Move]:
+        return []
+
 
 class King(Piece):
     value = 100
 
     def __init__(self, color: str, row: int, col: int):
         super().__init__(color=color, row=row, col=col)
+
+    def possible_moves(self, board) -> List[Move]:
+        return []
--- chess/state.py
+++ chess/state.py
@@ -1,12 +1,10 @@
 from move import Move
-from piece import Piece, Bishop, King, Knight, Pawn, Queen, Rook, Color
+from piece import Piece
+from square import Square
 
-from chess import (
-    BLACKS_STARTING_KINGS_ROW,
-    BLACKS_STARTING_PAWN_ROW,
-    WHITES_STARTING_KINGS_ROW,
-    WHITES_STARTING_PAWN_ROW,
-)
+from chess import (BLACKS_STARTING_KINGS_ROW, BLACKS_STARTING_PAWN_ROW,
+                   EMPTY_SQUARE, WHITES_STARTING_KINGS_ROW,
+                   WHITES_STARTING_PAWN_ROW)
 
 
 class GameState:
@@ -14,8 +12,6 @@ class GameState:
     Responsible for storing/updating all the information about the current state of the game.
     Update state (make moves), record move log, keep track of who's got the piece advantage etc."
     """
-
-    EMPTY_SQUARE = "**"
 
     def __init__(self):
         """
@@ -25,10 +21,10 @@ class GameState:
         self.board = [
             [*BLACKS_STARTING_KINGS_ROW],
             [*BLACKS_STARTING_PAWN_ROW],
-            [self.EMPTY_SQUARE for _ in range(8)],
-            [self.EMPTY_SQUARE for _ in range(8)],
-            [self.EMPTY_SQUARE for _ in range(8)],
-            [self.EMPTY_SQUARE for _ in range(8)],
+            [EMPTY_SQUARE for _ in range(8)],
+            [EMPTY_SQUARE for _ in range(8)],
+            [EMPTY_SQUARE for _ in range(8)],
+            [EMPTY_SQUARE for _ in range(8)],
             [*WHITES_STARTING_PAWN_ROW],
             [*WHITES_STARTING_KINGS_ROW],
         ]
@@ -48,28 +44,63 @@ class GameState:
     # TODO: Update it to work with pawn promotion/en passant
     def make_move(self, move: Move) -> None:
         """ Move a piece on the chess board """
+        valid_moves = self.get_valid_moves()
+
+        if move in valid_moves:
+            self._update_board_state(move=move)
+
+    def _update_board_state(self, move: Move) -> None:
+        """ Update the boards state based on the move """
         # Makes original spot empty since we're moving the piece
+        piece = move.piece_to_move
+
         self._make_square_empty(row=move.start_row, col=move.start_col)
-        self.board[move.dest_row][move.dest_col] = move.piece_to_move  # Move the piece
+        self.board[move.dest_row][move.dest_col] = piece  # Move the piece
+        piece.update_position(destination_square=Square(row=move.dest_row, col=move.dest_col))
+        piece.moves_made += 1
+
+        print(piece.pos)
 
         print(move)
         self.move_log.append(move)
         self.white_turn = not self.white_turn
 
-        # Print the move!
-
-    def undo_move(self):
+    def undo_move(self) -> None:
         """ Undo the previous move """
         if not self.move_log:
             return
         previous_move = self.move_log.pop()
         self.board[previous_move.start_row][previous_move.start_col] = previous_move.piece_to_move
         self.board[previous_move.dest_row][previous_move.dest_col] = previous_move.piece_to_capture
-        # TODO: Switch turn back since we undo'd a move.
+        self.white_turn = not self.white_turn  # Switch the turn back since we undo'd a move
 
-    def redo_move(self):
+    def redo_move(self) -> None:
         pass
+
+    # If the user makes a move, and the game state changes, we should regenerate all moves
+    # In order to validate the move of a black, you need to generate all of whites possible moves.
+    # Make the move, generate all possible moves the opposing player, see if any of the moves attack your king
+    # If your king is safe, it is a valid move, and add it to the list, return list of valid moves
+    def get_valid_moves(self):
+        """ All moves considering check """
+        all_moves = self.get_all_possible_moves()
+        return all_moves
+
+    def get_all_possible_moves(self):
+        """
+        All moves without considering check
+        Iterate over the entire board evaluating all pieces for the current player
+        """
+        moves = []
+        n = len(self.board)
+        for row in range(n):
+            for col in range(n):
+                piece = self.board[row][col]
+                if piece != EMPTY_SQUARE:
+                    moves.extend(piece.possible_moves(board=self.board))
+
+        return moves
 
     def _make_square_empty(self, row: int, col: int) -> None:
         """ Render the square empty on the given row/col"""
-        self.board[row][col] = self.EMPTY_SQUARE
+        self.board[row][col] = EMPTY_SQUARE
